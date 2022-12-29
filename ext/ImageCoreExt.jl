c2357203897e1e81c72794f1b95924f75de2f608
Johnny Chen <johnnychen94@hotmail.com>
2022-12-29 23:37:21 +0800

    switch to Extensions
    This also split out a test/Project.toml file for easy management

--- ext/ImageCoreExt.jl
+++ ext/ImageCoreExt.jl
@@ -1,3 +1,20 @@
+module ImageCoreExt
+
+@static if isdefined(Base, :get_extension)
+    using ImageCore
+else
+    using ..ImageCore
+end
+
+using MultiChannelColors: ColorMixture
+
 # clamp01 is designed to work on RGB colors (a major usage is for display), so convert first
 ImageCore.clamp01(c::ColorMixture{T}) where T = convert(RGB{T}, ImageCore.clamp01(convert(RGB{floattype(T)}, c)))
 ImageCore.clamp01nan(c::ColorMixture{T}) where T = convert(RGB{T}, ImageCore.clamp01nan(convert(RGB{floattype(T)}, c)))
+
+function __init__()
+    @debug "ImageCoreExt loaded"
+    return nothing
+end
+
+end
